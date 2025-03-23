0b35e1192deb5276357b7648aa9870f05cc25077
MattyGam3r <matthew_botterill@outlook.com>
2025-03-23 21:46:49 +1030

    added a basic app format, started python backend

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,30 @@
+import os
+import sqlite3
 from flask import Flask, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
 
+#Store Uploads Here:
+UPLOAD_FOLDER = "uploads"
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+
+#Sqlite database to hold the images
+DATABASE = 'metadata.db'
+
 @app.route("/")
 def home():
     return jsonify({"message": "Hello from Flask!"})
+
+def init_db():
+
+
+
+@app.route("/upload", methods = ["POST"])
+def upload_image():
+
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
--- data_annotator/lib/main.dart
+++ data_annotator/lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,12 +11,143 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return MaterialApp(
+      title: "Image Annotator",
+      theme: appTheme,
+      home: HomePage(),
+    );
+  }
+
+  static ThemeData appTheme = ThemeData(
+      //Default brightness and colours
+      colorScheme: ColorScheme.fromSeed(
+        seedColor: Colors.cyanAccent,
+        brightness: Brightness.light,
+      ).copyWith(
+        primary: Colors.amber,
       ),
+      //Text themes
+      textTheme: TextTheme(
+        displayLarge: const TextStyle(
+          fontSize: 72,
+          fontWeight: FontWeight.bold,
+        ),
+
+        titleLarge: GoogleFonts.roboto(
+          fontSize: 30,
+          fontStyle: FontStyle.italic,
+        ),
+        bodyMedium: GoogleFonts.roboto(),
+        displaySmall: GoogleFonts.pacifico(),
+      )
+    );
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+      
+        title: Text("Data Annotator"),
+        backgroundColor: Theme.of(context).colorScheme.primary,
+      ),
+      body: Row(
+        children: [
+          ImageViewer(),
+          ImageLabellerArea()
+        ],
+      ),
+    );
+  }
+}
+
+class ImageViewer extends StatelessWidget {
+  const ImageViewer({
+    super.key,
+  });
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(children: [
+          Text("Select an Image to Label Here"),
+          SizedBox(width: 20),
+          FileUploadButton(),
+        ],),
+        ClickableImage(),
+        
+        Placeholder(),
+      ],
+    );
+  }
+}
+
+class ImageLabellerArea extends StatelessWidget {
+  const ImageLabellerArea({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      flex: 2,
+      child: Column(
+        children: [
+          Center(child: Text("Hello 2!")),
+          SizedBox(height: 10),
+          Image.asset("/home/matthew/Github/Data-Annotator/data_annotator/assets/DogSample1.png"),
+        ],
+        ),
+    );
+  }
+}
+
+class ClickableImage extends StatelessWidget {
+  const ClickableImage({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    var image = NetworkImage("https://picsum.photos/350/200");
+    return InkWell(
+      onTap: () {      
+        print("clicked!");
+      },
+      child: Ink.image(
+        fit: BoxFit.cover,
+        width: 100,
+        height: 100,
+        image: image,
+      )
+    );
+  }
+}
+
+class FileUploadButton extends StatelessWidget {
+  const FileUploadButton({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      child: Text('Upload Image(s)'),
+      onPressed: () async {
+        var picked = await FilePicker.platform.pickFiles(
+          allowMultiple: true,
+        );
+
+        if (picked != null) {
+          print(picked.files.first.name);
+        }
+        print(picked);
+      },
     );
   }
 }
